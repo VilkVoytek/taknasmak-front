ee9a565ae36bd1a332b30a88c8218000cf9c5306
Wojciech Brozyna <vilk.voytek@gmail.com>
2020-01-09 11:52:49 +0100

    # index.js

--- src/tak-studio/taknasmak/overrideMapping.js
+++ src/tak-studio/taknasmak/overrideMapping.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 const veniaUiLib = path.resolve(`${process.cwd()}/../../pwa-studio/packages/venia-ui/lib`);
+const veniaConcept = path.resolve(`${process.cwd()}/../../pwa-studio/packages/venia-concept`);
 const peregrineLib = path.resolve(process.cwd() + '/../../pwa-studio/packages/peregrine/lib');
 
 module.exports = componentOverride = {
+    [`${veniaConcept}/src`]: 'src/lib',
     [`${veniaUiLib}/components/Header`]: 'src/lib/components/Header',
     [`${veniaUiLib}/components/Main`]: 'src/lib/components/Main',
     [`${veniaUiLib}/components/Footer`]: 'src/lib/components/Footer',
